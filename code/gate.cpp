7e67106669a36f5f7cbe0bdb799bf9e1277dd3b3
Ishwor Basyal <ishwor8basyal@gmail.com>
2022-07-08 20:34:09 +0545

    Dont know what

---
+++ code/gate.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "SDL_image.h"
+
+class Gate
+{
+    SDL_Rect output;
+    
+};
---
+++ code/graphics.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "SDL.h"
+#include "SDL_image.h"
+
+enum{
+	CLOSED, 
+};
+
+
+class Graphics {
+	SDL_Window* window;
+	SDL_Surface* loadingSurface;
+	SDL_Renderer* renderer;
+	SDL_Texture * texture;
+	bool isRunning;
+public:
+	Graphics();
+	~Graphics();
+	int mainLoop();
+	Uint32 getTime();
+	void delay(Uint32 ms);
+	void display();
+	void loadSprite();
+	void clearScreen(Uint8 r, Uint8 g, Uint8 b);
+
+};
+
